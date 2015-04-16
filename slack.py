782035d000d43dafa67d3166789d0e19aa1b91c5
Steve Black <sblack@paytronix.com>
2015-04-16 16:52:44 -0400

    tabz-be-gone

--- slack.py
+++ slack.py
@@ -15,34 +15,34 @@ os.chdir(os.path.dirname(sys.argv[0]))
 argParser = argparse.ArgumentParser(description="Notify Slack channel or user with message built from (potentially filtered) text piped in")
 
 argParser.add_argument(
-	'-c', '--slack-channel',
-	required=True,
-	action="store",
-	dest='channel',
-	help='Slack channel or user to notify; ex) @bob for a DM or #public-channel for a channel post'
+    '-c', '--slack-channel',
+    required=True,
+    action="store",
+    dest='channel',
+    help='Slack channel or user to notify; ex) @bob for a DM or #public-channel for a channel post'
 )
 
 argParser.add_argument(
-	'-m', '--max-lines',
-	action="store",
-	dest='maxLines',
-	default=1,
-	help='Maximum number of lines to add to Slack notification; when used with -r, only add the specified number of lines that match the regular expression provided'
+    '-m', '--max-lines',
+    action="store",
+    dest='maxLines',
+    default=1,
+    help='Maximum number of lines to add to Slack notification; when used with -r, only add the specified number of lines that match the regular expression provided'
 )
 
 argParser.add_argument(
-	'--config-path',
-	action="store",
-	dest='configPath',
-	default='~/.pipe2slack',
-	help='Path to configuration file for this script; default is ~/.pipe2slack'
+    '--config-path',
+    action="store",
+    dest='configPath',
+    default='~/.pipe2slack',
+    help='Path to configuration file for this script; default is ~/.pipe2slack'
 )
 
 argParser.add_argument(
-	'-r', '--regex',
-	action="store",
-	dest='regex',
-	help='Regular expression to match against input'
+    '-r', '--regex',
+    action="store",
+    dest='regex',
+    help='Regular expression to match against input'
 )
 
 args = argParser.parse_args(sys.argv[1:])
@@ -56,47 +56,47 @@ config = ConfigParser.RawConfigParser()
 expandedConfigPath = os.path.expanduser(configPath)
 
 if not config.read(expandedConfigPath):
-	sys.stderr.write('No configuration file present at {0}\n'.format(expandedConfigPath))
-	sys.exit(1)
+    sys.stderr.write('No configuration file present at {0}\n'.format(expandedConfigPath))
+    sys.exit(1)
 
 def getConfigSection(section):
-	return config.items(section)
+    return config.items(section)
 
 def getConfigOption(section, option):
-	return config.get(section, option)
+    return config.get(section, option)
 
 iconEmoji = getConfigOption('slack', 'emoji')
 webhookURL = getConfigOption('slack', 'webhookURL')
 
 def packageAndSend(message):
-	payload = {
-		'channel':		channel,
-		'username':		socket.gethostname(),
-		'text':			message,
-		'icon_emoji':	iconEmoji
-	}
-	requests.post(webhookURL, data=json.dumps(payload))
+    payload = {
+        'channel':       channel,
+        'username':      socket.gethostname(),
+        'text':          message,
+        'icon_emoji':    iconEmoji
+    }
+    requests.post(webhookURL, data=json.dumps(payload))
 
 matchedLineCount = 0
 text = ""
 
 if regexString:
-	regex = re.compile(regexString)
-	for line in sys.stdin:
-		sys.stdout.write(line)
-		if matchedLineCount < maxLines:
-			if regex.search(line):
-				text += line
-				matchedLineCount += 1
-				if matchedLineCount == maxLines:
-					packageAndSend(text)
-	sys.exit(0)
+    regex = re.compile(regexString)
+    for line in sys.stdin:
+        sys.stdout.write(line)
+        if matchedLineCount < maxLines:
+            if regex.search(line):
+                text += line
+                matchedLineCount += 1
+                if matchedLineCount == maxLines:
+                    packageAndSend(text)
+    sys.exit(0)
 else:
-	for line in sys.stdin:
-		sys.stdout.write(line)
-		if matchedLineCount < maxLines:
-			text += line
-			matchedLineCount += 1
-			if matchedLineCount == maxLines:
-				packageAndSend(text)
-	sys.exit(0)
+    for line in sys.stdin:
+        sys.stdout.write(line)
+        if matchedLineCount < maxLines:
+            text += line
+            matchedLineCount += 1
+            if matchedLineCount == maxLines:
+                packageAndSend(text)
+    sys.exit(0)
